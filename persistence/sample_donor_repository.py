183a38a384362f252a7eeb104b88e7ef06f98aa3
Radovan Tomasik <tomasik.radovan@gmail.com>
2023-06-09 14:07:44 +0200

    Add todo for repository implementation

--- persistence/sample_donor_repository.py
+++ persistence/sample_donor_repository.py
@@ -2,7 +2,7 @@ from typing import List
 
 from model.sample_donor import SampleDonor
 
-
+# TODO implement
 class SampleDonorRepository:
     def get_all(self) -> List[SampleDonor]:
         return []
