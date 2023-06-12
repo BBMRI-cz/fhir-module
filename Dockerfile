68f45985b2fedfd2e2d07d0baf1de4ddc9b5122b
Radovan Tomasik <tomasik.radovan@gmail.com>
2023-06-12 12:57:19 +0200

    Add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.11-alpine
+RUN apk add --no-cache --upgrade bash
+ENV APP_DIR="/opt/fhir-module"
+ENV RECORDS_DIR="/opt/records"
+RUN mkdir -p $APP_DIR $RECORDS_DIR
+WORKDIR $APP_DIR
+COPY --chown=1001:1001 . .
+RUN pip install --no-cache-dir -r requirements.txt
+USER 1001
+CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from service.blaze_service import BlazeService
 from service.patient_service import PatientService
 
 if __name__ == "__main__":
-    blaze_service = BlazeService(PatientService(SampleDonorXMLFilesRepository()), os.getenv("BLAZE_URL"))
+    blaze_service = BlazeService(PatientService(SampleDonorXMLFilesRepository()),
+                                 os.getenv("BLAZE_URL", "http://localhost:8080/fhir/"))
     schedule.every().day.at("10:00").do(blaze_service.initial_upload_of_all_patients)
     while True:
         schedule.run_pending()
