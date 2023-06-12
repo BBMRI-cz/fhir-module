3bd57c55e2de83c0c8b2e34ac81f16ccdea064d1
Radovan Tomasik <tomasik.radovan@gmail.com>
2023-06-12 08:41:25 +0200

    Refactor XML repo impl

--- persistence/sample_donor_xml_files_repository.py
+++ persistence/sample_donor_xml_files_repository.py
@@ -1,5 +1,4 @@
 import os
-from pyexpat import ExpatError
 from typing import List
 
 import xmltodict as xmltodict
@@ -9,13 +8,13 @@ from persistence.sample_donor_repository import SampleDonorRepository
 
 
 class SampleDonorXMLFilesRepository(SampleDonorRepository):
-    _dir_path = "/Users/radovan.tomasik/Repositories/fhir-module/test/xml_data"
+    _dir_path = os.getenv("DIR_PATH")
 
     def get_all(self) -> List[SampleDonor]:
         for dirEntry in os.scandir(self._dir_path):
-            yield from self.parse_xml_file(dirEntry)
+            yield self.parse_xml_file(dirEntry)
 
     def parse_xml_file(self, dir_entry: os.DirEntry):
         with open(dir_entry) as xml_file:
             file_content = xmltodict.parse(xml_file.read())
-            yield SampleDonor(file_content.get("patient", {}).get("@id", {}))
+            return SampleDonor(file_content.get("patient", {}).get("@id", {}))
