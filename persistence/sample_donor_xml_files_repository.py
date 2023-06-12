a2dc84b6467943c8fdfd084386c8d90e4c6c64fa
Radovan Tomasik <tomasik.radovan@gmail.com>
2023-06-12 11:46:45 +0200

    Add handling of duplicate patient files

--- persistence/sample_donor_xml_files_repository.py
+++ persistence/sample_donor_xml_files_repository.py
@@ -1,4 +1,5 @@
 import os
+from pyexpat import ExpatError
 from typing import List
 
 import xmltodict as xmltodict
@@ -8,13 +9,22 @@ from persistence.sample_donor_repository import SampleDonorRepository
 
 
 class SampleDonorXMLFilesRepository(SampleDonorRepository):
-    _dir_path = os.getenv("DIR_PATH", "/mock_dir/")
+
+    def __init__(self):
+        self._dir_path = os.getenv("DIR_PATH", "/mock_dir/")
+        self._ids: set = set()
 
     def get_all(self) -> List[SampleDonor]:
         for dirEntry in os.scandir(self._dir_path):
-            yield self.parse_xml_file(dirEntry)
+            yield from self.parse_xml_file(dirEntry)
 
     def parse_xml_file(self, dir_entry: os.DirEntry):
         with open(dir_entry) as xml_file:
-            file_content = xmltodict.parse(xml_file.read())
-            return SampleDonor(file_content.get("patient", {}).get("@id", {}))
+            try:
+                file_content = xmltodict.parse(xml_file.read())
+                donor = SampleDonor(file_content.get("patient", {}).get("@id", {}))
+                if donor.identifier not in self._ids:
+                    self._ids.add(donor.identifier)
+                    yield donor
+            except ExpatError:
+                print("Skipping file")
--- test/integration/test_xml_repository.py
+++ test/integration/test_xml_repository.py
@@ -1,37 +1,38 @@
 import unittest
-
-import pytest
-from pyfakefs.fake_filesystem_unittest import TestCase
+from pyfakefs.fake_filesystem_unittest import patchfs
 from model.sample_donor import SampleDonor
 from persistence.sample_donor_xml_files_repository import SampleDonorXMLFilesRepository
 
 
-class TestXMLRepo(TestCase):
-    sample_donor_repository = SampleDonorXMLFilesRepository()
+class TestXMLRepo(unittest.TestCase):
     content = '<patient xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' \
               'xmlns="http://www.bbmri.cz/schemas/biobank/data" xsi:noNamespaceSchemaLocation="exportNIS.xsd" ' \
               'id="9999"></patient>'
+    dir_path = "/mock_dir/"
 
-    @classmethod
-    def setUpClass(cls) -> None:
-        cls._fileParser = None
-        cls.setUpClassPyfakefs()
-        cls.dir_path = "/mock_dir/"
-
-    @pytest.fixture(autouse=True)
-    def runBeforeAndAfterEachTest(self):
-        self.fs.create_dir(self.dir_path)
-        # Execute test
-        yield
-        self._fileParser = None
-        self.fs.reset()
-
-    def test_get_all(self):
-        self.fs.create_file(self.dir_path + "mock_file.xml", contents=self.content)
-        for donor in self.sample_donor_repository.get_all():
+    @patchfs
+    def test_get_all(self, fake_fs):
+        fake_fs.create_file(self.dir_path + "mock_file.xml", contents=self.content)
+        for donor in SampleDonorXMLFilesRepository().get_all():
             self.assertIsInstance(donor, SampleDonor)
             self.assertEqual("9999", donor.identifier)
 
+    @patchfs
+    def test_get_all_with_one_wrongly_formatted_file(self, fake_fs):
+        fake_fs.create_file(self.dir_path + "mock_file.xml", contents=self.content)
+        fake_fs.create_file(self.dir_path + "mock_wrong_file.xml", contents="<>test</a>")
+        for donor in SampleDonorXMLFilesRepository().get_all():
+            self.assertIsInstance(donor, SampleDonor)
+            self.assertEqual("9999", donor.identifier)
 
-if __name__ == '__main__':
-    unittest.main()
+    @patchfs
+    def test_get_all_does_not_return_duplicate_patients(self, fake_fs):
+        fake_fs.create_file(self.dir_path + "mock_file.xml", contents=self.content)
+        fake_fs.create_file(self.dir_path + "mock_file_duplicate.xml", contents=self.content)
+        counter = 0
+        for donor in SampleDonorXMLFilesRepository().get_all():
+            self.assertIsInstance(donor, SampleDonor)
+            self.assertEqual("9999", donor.identifier)
+            counter += 1
+        self.assertEqual(1, counter)
+
