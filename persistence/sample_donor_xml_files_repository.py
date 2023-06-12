a6632c2b8a287a59cf5dd083576173eb3ccba258
Radovan Tomasik <tomasik.radovan@gmail.com>
2023-06-12 08:36:22 +0200

    Add basic implementation of XML repository

--- persistence/sample_donor_xml_files_repository.py
+++ persistence/sample_donor_xml_files_repository.py
@@ -1,9 +1,21 @@
+import os
+from pyexpat import ExpatError
 from typing import List
+
+import xmltodict as xmltodict
 
 from model.sample_donor import SampleDonor
 from persistence.sample_donor_repository import SampleDonorRepository
 
 
 class SampleDonorXMLFilesRepository(SampleDonorRepository):
+    _dir_path = "/Users/radovan.tomasik/Repositories/fhir-module/test/xml_data"
+
     def get_all(self) -> List[SampleDonor]:
-        pass
+        for dirEntry in os.scandir(self._dir_path):
+            yield from self.parse_xml_file(dirEntry)
+
+    def parse_xml_file(self, dir_entry: os.DirEntry):
+        with open(dir_entry) as xml_file:
+            file_content = xmltodict.parse(xml_file.read())
+            yield SampleDonor(file_content.get("patient", {}).get("@id", {}))
--- test/integration/test_xml_repository.py
+++ test/integration/test_xml_repository.py
@@ -1,5 +1,6 @@
 import unittest
 
+from model.sample_donor import SampleDonor
 from persistence.sample_donor_xml_files_repository import SampleDonorXMLFilesRepository
 
 
@@ -7,7 +8,9 @@ class TestXMLRepo(unittest.TestCase):
     sample_donor_repository = SampleDonorXMLFilesRepository()
 
     def test_get_all(self):
-        self.assertEqual(True, False)  # add assertion here
+        for donor in self.sample_donor_repository.get_all():
+            self.assertIsInstance(donor, SampleDonor)
+            self.assertEqual("9999", donor.identifier)
 
 
 if __name__ == '__main__':
