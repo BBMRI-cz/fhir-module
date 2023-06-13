a79508a52a2dea25afad2bff9b934ed0da6c771b
Radovan Tomasik <tomasik.radovan@gmail.com>
2023-06-13 12:00:33 +0200

    Add condition for turning on the patient upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import time
 
+import requests
 import schedule
 
 from persistence.sample_donor_xml_files_repository import SampleDonorXMLFilesRepository
@@ -8,9 +9,13 @@ from service.blaze_service import BlazeService
 from service.patient_service import PatientService
 
 if __name__ == "__main__":
+    res = requests.get(url="http://localhost:8080/fhir/Patient?_summary=count")
     blaze_service = BlazeService(PatientService(SampleDonorXMLFilesRepository()),
                                  os.getenv("BLAZE_URL", "http://localhost:8080/fhir/"))
-    schedule.every().day.at("10:00").do(blaze_service.initial_upload_of_all_patients)
+    if res.text.find("total") == 0:
+        blaze_service.initial_upload_of_all_patients()
+    else:
+        print("Patients already present in the FHIR store.")
     while True:
         schedule.run_pending()
         time.sleep(1)
