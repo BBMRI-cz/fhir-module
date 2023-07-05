81388ea71b2a55de371135488bd388d8a2ac3216
RadovanTomik <tomasik.radovan@gmail.com>
2023-07-05 20:26:40 +0200

    Rename variable to snake case

--- util/custom_logger.py
+++ util/custom_logger.py
@@ -8,6 +8,6 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def setup_logger():
-    with open(os.path.join(ROOT_DIR, 'logging.yaml'), 'r') as f:
-        log_cfg = yaml.safe_load(f.read())
+    with open(os.path.join(ROOT_DIR, 'logging.yaml'), 'r') as config_file:
+        log_cfg = yaml.safe_load(config_file.read())
         logging.config.dictConfig(log_cfg)
