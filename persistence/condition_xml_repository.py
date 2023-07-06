e75d99e2ab6ac46dbad81e8de45d63887550363d
Radovan Tomasik <tomasik.radovan@gmail.com>
2023-07-06 16:55:05 +0200

    Add basic implementation of ConditionXMLRepository

--- persistence/condition_xml_repository.py
+++ persistence/condition_xml_repository.py
@@ -1,5 +1,9 @@
 """Module containing classes tied to condition persistence in XML files"""
+import os
+from pyexpat import ExpatError
 from typing import List
+
+import xmltodict
 
 from model.condition import Condition
 from persistence.condition_repository import ConditionRepository
@@ -7,5 +11,24 @@ from persistence.condition_repository import ConditionRepository
 
 class ConditionXMLRepository(ConditionRepository):
     """Class for handling condition persistence in XML files"""
+
+    def __init__(self):
+        self._dir_path = os.getenv("DIR_PATH", "/mock_dir/")
+
     def get_all(self) -> List[Condition]:
-        pass
+        for dir_entry in os.scandir(self._dir_path):
+            yield from self.parse_xml_file(dir_entry)
+
+    def parse_xml_file(self, dir_entry: os.DirEntry) -> Condition:
+        """Parse a Sample donor from an XML file"""
+        with open(dir_entry, encoding="UTF-8") as xml_file:
+            try:
+                file_content = xmltodict.parse(xml_file.read())
+                condition = Condition(patient_id=file_content.get("patient", {}).get("@id", {}),
+                                      icd_10_code=file_content.get("patient", {})
+                                      .get("STS", {})
+                                      .get("diagnosisMaterial", {})
+                                      .get("diagnosis", {}))
+                yield condition
+            except ExpatError:
+                print("Skipping file")
--- test/unit/test_condition_xml_repository.py
+++ test/unit/test_condition_xml_repository.py
@@ -2,6 +2,9 @@ import unittest
 
 from pyfakefs.fake_filesystem_unittest import patchfs
 
+from model.condition import Condition
+from persistence.condition_xml_repository import ConditionXMLRepository
+
 
 class TestConditionXMLRepository(unittest.TestCase):
     samples = '<STS>' \
@@ -9,14 +12,17 @@ class TestConditionXMLRepository(unittest.TestCase):
               '<diagnosis>C509</diagnosis>' \
               '</diagnosisMaterial>'
     '</STS>'
-    content = ('<patient xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' \
-               'xmlns="http://www.bbmri.cz/schemas/biobank/data" xsi:noNamespaceSchemaLocation="exportNIS.xsd" ' \
-               'id="9999">%s</patient>', samples)
+    content = '<patient xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' \
+              'xmlns="http://www.bbmri.cz/schemas/biobank/data" xsi:noNamespaceSchemaLocation="exportNIS.xsd" ' \
+              'id="9999">{sample}</patient>'.format(sample=samples)
     dir_path = "/mock_dir/"
 
     @patchfs
     def test_get_all(self, fake_fs):
         fake_fs.create_file(self.dir_path + "mock_file.xml", contents=self.content)
+        for condition in ConditionXMLRepository().get_all():
+            self.assertIsInstance(condition, Condition)
+            self.assertEqual("C509", condition.icd_10_code)
 
 
 if __name__ == '__main__':
