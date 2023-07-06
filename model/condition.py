417d73a4a4e6f22acf0c873cc1c5a1ba45473dba
RadovanTomik <tomasik.radovan@gmail.com>
2023-07-06 07:35:17 +0200

    Add ICD-10 validation

--- model/condition.py
+++ model/condition.py
@@ -1,8 +1,11 @@
-"""Module for representing a patient's condition"""
+"""Module for representing a patient's condition."""
+import icd10
 
 
 class Condition:
-    """Class representing a patient's medical condition"""
+    """Class representing a patient's medical condition using ICD-10 coding."""
     def __init__(self, icd_10_code: str, patient_id: str):
+        if not icd10.exists(icd_10_code):
+            raise TypeError("The provided string is not a valid ICD-10 code.")
         self.icd_10_code = icd_10_code
         self.patient_id = patient_id
--- test/unit/test_condition.py
+++ test/unit/test_condition.py
@@ -9,3 +9,10 @@ class TestCondition(unittest.TestCase):
         condition = Condition("C18.8", "patient-ID")
         self.assertIsInstance(condition, Condition)
 
+    def test_wrong_icd_10_code_throws_error(self):
+        with self.assertRaises(TypeError):
+            Condition("C7777", "patient-ID")
+        with self.assertRaises(TypeError):
+            Condition("77", "patient-ID")
+        with self.assertRaises(TypeError):
+            Condition("rand_string", "patient-ID")
