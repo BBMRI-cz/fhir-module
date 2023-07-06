9157b77ade62d72701c2c36cda9e8c9baad3185a
RadovanTomik <tomasik.radovan@gmail.com>
2023-07-06 08:17:50 +0200

    Add icd-10 code to FHIR condition

--- model/condition.py
+++ model/condition.py
@@ -1,11 +1,39 @@
 """Module for representing a patient's condition."""
+import fhirclient.models.codeableconcept
+import fhirclient.models.coding
+import fhirclient.models.meta
+import fhirclient.models.condition as fhir_condition
 import icd10
 
 
 class Condition:
     """Class representing a patient's medical condition using ICD-10 coding."""
+
     def __init__(self, icd_10_code: str, patient_id: str):
         if not icd10.exists(icd_10_code):
             raise TypeError("The provided string is not a valid ICD-10 code.")
-        self.icd_10_code = icd_10_code
-        self.patient_id = patient_id
+        self._icd_10_code = icd_10_code
+        self._patient_id = patient_id
+
+    @property
+    def icd_10_code(self) -> str:
+        """Get ICD-10 code"""
+        return self._icd_10_code
+
+    def to_fhir(self) -> fhir_condition.Condition:
+        """Return condition's representation as a FHIR resource."""
+        condition = fhir_condition.Condition()
+        condition.meta = fhirclient.models.meta.Meta()
+        condition.meta.profile = ["https://fhir.bbmri.de/StructureDefinition/Condition"]
+        condition.code = fhirclient.models.codeableconcept.CodeableConcept()
+        condition.code.coding = fhirclient.models.coding.Coding()
+        condition.code.coding.code = self.__icd_10_code_with_period()
+        condition.code.coding.system = "http://hl7.org/fhir/sid/icd-10"
+        return condition
+
+    def __icd_10_code_with_period(self) -> str:
+        """Returns icd-10 code with period, e.g. C188 to C18.8"""
+        code = self._icd_10_code
+        if len(code) == 4 and "." not in code:
+            return code[:3] + '.' + code[3:]
+        return code
--- test/unit/test_condition.py
+++ test/unit/test_condition.py
@@ -1,5 +1,7 @@
 import unittest
 
+import fhirclient.models.condition
+
 from model.condition import Condition
 
 
@@ -16,3 +18,21 @@ class TestCondition(unittest.TestCase):
             Condition("77", "patient-ID")
         with self.assertRaises(TypeError):
             Condition("rand_string", "patient-ID")
+
+    def test_get_icd_10_code(self):
+        condition = Condition("C18.8", "patient-ID")
+        self.assertEqual("C18.8", condition.icd_10_code)
+        condition = Condition("C188", "patient-ID")
+        self.assertEqual("C188", condition.icd_10_code)
+
+    def test_to_fhir(self):
+        condition = Condition("C18.8", "patient-ID")
+        self.assertIsInstance(condition.to_fhir(), fhirclient.models.condition.Condition)
+
+    def test_to_fhir_valid_icd_10_code(self):
+        condition = Condition("C18.8", "patient-ID")
+        self.assertEqual("C18.8", condition.to_fhir().code.coding.code)
+
+    def test_to_fhir_period_is_added_to_icd_10_code(self):
+        condition = Condition("C188", "patient-ID")
+        self.assertEqual("C18.8", condition.to_fhir().code.coding.code)
