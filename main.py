e552bf8769b9597c386ba7b9f8fb722bae4bfd00
Radovan Tomasik <tomasik.radovan@gmail.com>
2023-07-18 14:07:40 +0200

    Fix missing arguments in main

--- main.py
+++ main.py
@@ -5,16 +5,19 @@ import time
 
 import schedule
 
+from persistence.condition_xml_repository import ConditionXMLRepository
 from persistence.sample_donor_xml_files_repository import SampleDonorXMLFilesRepository
 from service.blaze_service import BlazeService
+from service.condition_service import ConditionService
 from service.patient_service import PatientService
 from util.custom_logger import setup_logger
 
 if __name__ == "__main__":
     setup_logger()
     logger = logging.getLogger(__name__)
-    blaze_service = BlazeService(PatientService(SampleDonorXMLFilesRepository()),
-                                 os.getenv("BLAZE_URL", "http://localhost:8080/fhir"))
+    blaze_service = BlazeService(patient_service=PatientService(SampleDonorXMLFilesRepository()),
+                                 condition_service=ConditionService(ConditionXMLRepository()),
+                                 blaze_url=os.getenv("BLAZE_URL", "http://localhost:8080/fhir"))
     logger.info("Successfully started FHIR_Module!")
     if blaze_service.get_num_of_patients() == 0:
         logger.info("Starting upload of patients...")
