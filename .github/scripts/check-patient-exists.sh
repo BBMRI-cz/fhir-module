1a347e469f55ab3ae5c932595c8fcc7682734990
Radovan Tomasik <tomasik.radovan@gmail.com>
2023-08-10 11:16:59 +0200

    test: add specimen check to system test

--- .github/scripts/check-patient-exists.sh
+++ .github/scripts/check-patient-exists.sh
@@ -2,6 +2,7 @@
 
 NUM_OF_PATIENTS=$(curl -X GET http://localhost:8080/fhir/Patient?_summary=count | jq .total)
 NUM_OF_CONDITIONS=$(curl -X GET http://localhost:8080/fhir/Condition?_summary=count | jq .total)
-if [ "$NUM_OF_PATIENTS" = 0 ] || [ "$NUM_OF_CONDITIONS" = 0 ]; then
-	echo "The upload of patients or conditions failed." && exit 1
+NUM_OF_SPECIMENS=$(curl -X GET http://localhost:8080/fhir/Specimen?_summary=count | jq .total)
+if [ "$NUM_OF_PATIENTS" = 0 ] || [ "$NUM_OF_CONDITIONS" = 0 ] || [ "$NUM_OF_SPECIMENS" = 0 ]; then
+	echo "The upload of resources failed." && exit 1
 fi
