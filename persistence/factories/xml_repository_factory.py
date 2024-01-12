b582b46266cc8b3e00191eadb10d04c9e2703985
SimonKonar <simon.konar@gmail.com>
2024-01-12 15:16:41 +0100

    feat: add RepositoryFactoryfor creating concrete(XML or CSV) repositories

---
+++ persistence/factories/xml_repository_factory.py
@@ -0,0 +1,28 @@
+from persistence.condition_repository import ConditionRepository
+from persistence.condition_xml_repository import ConditionXMLRepository
+from persistence.factories.repository_factory import RepositoryFactory
+from persistence.sample_collection_json_repository import SampleCollectionJSONRepository
+from persistence.sample_collection_repository import SampleCollectionRepository
+from persistence.sample_donor_repository import SampleDonorRepository
+from persistence.sample_donor_xml_files_repository import SampleDonorXMLFilesRepository
+from persistence.sample_repository import SampleRepository
+from persistence.sample_xml_repository import SampleXMLRepository
+from util.config import RECORDS_DIR_PATH, PARSING_MAP, SAMPLE_COLLECTIONS_PATH, TYPE_TO_COLLECTION_MAP
+
+
+class XMLRepositoryFactory(RepositoryFactory):
+    def create_condition_repository(self) -> ConditionRepository:
+        return ConditionXMLRepository(records_path=RECORDS_DIR_PATH,
+                                      condition_parsing_map=PARSING_MAP['condition_map'])
+
+    def create_sample_collection_repository(self) -> SampleCollectionRepository:
+        return SampleCollectionJSONRepository(SAMPLE_COLLECTIONS_PATH)
+
+    def create_sample_repository(self) -> SampleRepository:
+        return SampleXMLRepository(records_path=RECORDS_DIR_PATH,
+                                   sample_parsing_map=PARSING_MAP['sample_map'],
+                                   type_to_collection_map=TYPE_TO_COLLECTION_MAP)
+
+    def create_sample_donor_repository(self) -> SampleDonorRepository:
+        return SampleDonorXMLFilesRepository(records_path=RECORDS_DIR_PATH,
+                                             donor_parsing_map=PARSING_MAP['donor_map'])
