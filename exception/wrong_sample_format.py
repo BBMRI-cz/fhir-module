3b1f643104c3a5c558d7ce8a0c830901607d1b87
SimonKonar <simon.konar@gmail.com>
2024-01-19 11:24:51 +0100

    fix: moved exceptions from utils to separate files

---
+++ exception/wrong_sample_format.py
@@ -0,0 +1,3 @@
+class WrongSampleMapException(Exception):
+    """Raised when the sample map being read has a wrong format"""
+    pass
--- persistence/csv_util.py
+++ persistence/csv_util.py
@@ -1,11 +1,7 @@
+from exception.wrong_sample_format import WrongSampleMapException
 
 
 def check_sample_map_format(sample_parsing_map: dict):
     """Check if sample_details is available in sample parsing map"""
     if sample_parsing_map.get("sample_details") is None:
-        raise WrongSampleMapException
-
-
-class WrongSampleMapException(Exception):
-    """Raised when the sample map being read has a wrong format"""
-    pass
+        raise WrongSampleMapException("Sample parsing map cannot be empty")
