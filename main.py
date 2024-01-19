1d563774187d6457741656009f6fd81798971fa6
SimonKonar <simon.konar@gmail.com>
2024-01-19 11:32:53 +0100

    fix: change blaze service constructor to use specific instances instead of factory

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 import logging
 import sys
 from service.blaze_service import BlazeService
+from service.condition_service import ConditionService
+from service.patient_service import PatientService
+from service.sample_service import SampleService
 from util.config import BLAZE_URL
 from util.custom_logger import setup_logger
 from util.http_util import is_endpoint_available
@@ -13,7 +16,14 @@ if __name__ == "__main__":
     logger.info("Starting FHIR_Module...")
     if is_endpoint_available(endpoint_url=BLAZE_URL, wait_time=10, max_attempts=5):
         repository_factory = get_repository_factory()
-        blaze_service = BlazeService(repository_factory=repository_factory, blaze_url=BLAZE_URL)
+        patient_service = PatientService(repository_factory.create_sample_donor_repository())
+        condition_service = ConditionService(repository_factory.create_condition_repository())
+        sample_service = SampleService(repository_factory.create_sample_repository())
+        sample_collection_repository = repository_factory.create_sample_collection_repository()
+        blaze_service = BlazeService(patient_service=patient_service, condition_service=condition_service,
+                                     sample_service=sample_service, blaze_url=BLAZE_URL,
+                                     sample_collection_repository=sample_collection_repository)
+        blaze_service.sync_samples()
         blaze_service.sync()
     else:
         logger.error("Exiting FHIR_Module.")
