c2f57a6f0880221f40e8783921bcc5f0cc2acce5
SimonKonar <simon.konar@gmail.com>
2024-11-04 14:29:40 +0100

    feat:allow to parse sample donors to miabis_on_fhir model representation

--- persistence/sample_donor_csv_repository.py
+++ persistence/sample_donor_csv_repository.py
@@ -5,8 +5,11 @@ from typing import List
 
 from dateutil.parser import ParserError
 
+from model.interface.sample_donor_interface import SampleDonorInterface
 from util.enums_util import get_gender_from_abbreviation
 from model.sample_donor import SampleDonor
+from model.miabis.sample_donor_miabis import SampleDonorMiabis
+from miabis_model.gender import get_gender_from_abbreviation as miabis_get_gender_from_abbreviation
 from persistence.sample_donor_repository import SampleDonorRepository
 from util.custom_logger import setup_logger
 from dateutil import parser as date_parser
@@ -18,43 +21,59 @@ logger = logging.getLogger()
 class SampleDonorCsvRepository(SampleDonorRepository):
     """Class for handling sample donors stored in Csv files"""
 
-    def __init__(self, records_path: str, separator: str, donor_parsing_map: dict):
+    def __init__(self, records_path: str, separator: str, donor_parsing_map: dict, miabis_on_fhir_model: bool = False):
         self._dir_path = records_path
         self._ids: set = set()
         self.separator = separator
         self._donor_parsing_map = donor_parsing_map
+        self._fields_dict = {}
+        self._miabis_on_fhir_model = miabis_on_fhir_model
         logger.debug(f"Loaded the following donor parsing map {donor_parsing_map}")
 
-    def get_all(self) -> List[SampleDonor]:
+    def get_all(self) -> List[SampleDonorInterface]:
         dir_entry: os.DirEntry
         for dir_entry in os.scandir(self._dir_path):
             if dir_entry.name.lower().endswith(".csv"):
                 yield from self.__extract_donor_from_csv_file(dir_entry)
 
-    def __extract_donor_from_csv_file(self, dir_entry: os.DirEntry) -> SampleDonor:
-        fields_dict: dict = {}
+    def __extract_donor_from_csv_file(self, dir_entry: os.DirEntry) -> SampleDonorInterface:
         with open(dir_entry, "r") as file_content:
             reader = csv.reader(file_content, delimiter=self.separator)
+            self._fields_dict = {}
             fields = next(reader)
             for i, field in enumerate(fields):
-                fields_dict[field] = i
+                self._fields_dict[field] = i
             for row in reader:
                 try:
-                    donor = SampleDonor(row[fields_dict[self._donor_parsing_map.get("id")]])
-                    donor.gender = get_gender_from_abbreviation(row[fields_dict[self._donor_parsing_map.get("gender")]])
-                    year_of_birth = row[fields_dict[self._donor_parsing_map.get("birthDate")]]
-                    if year_of_birth is not None:
-                        try:
-                            parsed_date = date_parser.parse(year_of_birth)
-                            # parsed_date = parse_date(year_of_birth)
-                        except ParserError:
-                            logger.warning(
-                                f"Error parsing date {year_of_birth}. Please make sure the date is in a valid format.")
-                            continue
-                        donor.date_of_birth = parsed_date
+                    donor = self.__build_donor(row)
                     if donor.identifier not in self._ids:
                         self._ids.add(donor.identifier)
                         yield donor
+                except ParserError as err:
+                    logger.info(f"{err}Skipping...")
+                    continue
                 except TypeError as err:
                     logger.info(f"{err} Skipping...")
                     continue
+
+    def __build_donor(self, data: list[str]) -> SampleDonorInterface:
+        identifier = data[self._fields_dict[self._donor_parsing_map.get("id")]]
+        gender_field = self._fields_dict[self._donor_parsing_map.get("gender")]
+        if gender_field is None:
+            raise ValueError(f"No gender provided for patient with identifier {identifier}")
+        gender = miabis_get_gender_from_abbreviation(data[gender_field])
+        birth_date_field = self._fields_dict[self._donor_parsing_map.get("birthDate")]
+        date_of_birth = None
+        if birth_date_field is not None:
+            try:
+                birth_date = date_parser.parse(data[birth_date_field])
+                date_of_birth = birth_date
+            except ParserError:
+                raise ParserError(f"Error while parsing donor with identifier {identifier}. "
+                                  f"Incorrect parsing date. Please make sure the date is in a valid format.")
+        if self._miabis_on_fhir_model:
+            donor = SampleDonorMiabis(identifier=identifier, gender=gender, birth_date=date_of_birth)
+        else:
+            donor = SampleDonor(identifier=identifier, gender=gender, birth_date=date_of_birth)
+        return donor
+
--- persistence/sample_donor_repository.py
+++ persistence/sample_donor_repository.py
@@ -2,12 +2,12 @@
 import abc
 from typing import List
 
-from model.sample_donor import SampleDonor
+from model.interface.sample_donor_interface import SampleDonorInterface
 
 
 class SampleDonorRepository(abc.ABC):
     """Class for handling a repository of Sample donors"""
 
     @abc.abstractmethod
-    def get_all(self) -> List[SampleDonor]:
+    def get_all(self) -> List[SampleDonorInterface]:
         """Fetches all SampleDonors in repository"""
