d955e3e00939ff4135c245da5b18f2742fdeb551
SimonKonar <simon.konar@gmail.com>
2024-11-04 14:22:57 +0100

    FEAT: add maibis on fhir model flag in repositories creation

--- persistence/factories/csv_repository_factory.py
+++ persistence/factories/csv_repository_factory.py
@@ -1,3 +1,5 @@
+from persistence.biobank_json_repository import BiobankJSONRepository
+from persistence.biobank_repository import BiobankRepository
 from persistence.condition_csv_repository import ConditionCsvRepository
 from persistence.condition_repository import ConditionRepository
 from persistence.factories.repository_factory import RepositoryFactory
@@ -7,27 +9,34 @@ from persistence.sample_csv_repository import SampleCsvRepository
 from persistence.sample_donor_csv_repository import SampleDonorCsvRepository
 from persistence.sample_donor_repository import SampleDonorRepository
 from persistence.sample_repository import SampleRepository
-from util.config import RECORDS_DIR_PATH, CSV_SEPARATOR, PARSING_MAP, SAMPLE_COLLECTIONS_PATH, TYPE_TO_COLLECTION_MAP,STORAGE_TEMP_MAP
+from util.config import RECORDS_DIR_PATH, CSV_SEPARATOR, PARSING_MAP, SAMPLE_COLLECTIONS_PATH, TYPE_TO_COLLECTION_MAP, \
+    STORAGE_TEMP_MAP, MATERIAL_TYPE_MAP_PATH, MATERIAL_TYPE_MAP, BIOBANK_PATH
 
 
 class CSVRepositoryFactory(RepositoryFactory):
     """This class instantiates repositories that work with csv files"""
-    def create_condition_repository(self) -> ConditionRepository:
+    def create_condition_repository(self, miabis_on_fhir_model: bool = False) -> ConditionRepository:
         return ConditionCsvRepository(records_path=RECORDS_DIR_PATH,
                                       separator=CSV_SEPARATOR,
                                       condition_parsing_map=PARSING_MAP['condition_map'])
 
-    def create_sample_collection_repository(self) -> SampleCollectionRepository:
-        return SampleCollectionJSONRepository(SAMPLE_COLLECTIONS_PATH)
+    def create_sample_collection_repository(self, miabis_on_fhir_model: bool = False) -> SampleCollectionRepository:
+        return SampleCollectionJSONRepository(SAMPLE_COLLECTIONS_PATH,miabis_on_fhir_model)
 
-    def create_sample_repository(self) -> SampleRepository:
+    def create_sample_repository(self, miabis_on_fhir_model: bool = False) -> SampleRepository:
         return SampleCsvRepository(records_path=RECORDS_DIR_PATH,
                                    sample_parsing_map=PARSING_MAP['sample_map'],
                                    separator=CSV_SEPARATOR,
                                    type_to_collection_map=TYPE_TO_COLLECTION_MAP,
-                                   storage_temp_map=STORAGE_TEMP_MAP)
+                                   storage_temp_map=STORAGE_TEMP_MAP,
+                                   material_type_map=MATERIAL_TYPE_MAP,
+                                   miabis_on_fhir_model=miabis_on_fhir_model)
 
-    def create_sample_donor_repository(self) -> SampleDonorRepository:
+    def create_sample_donor_repository(self, miabis_on_fhir_model: bool = False) -> SampleDonorRepository:
         return SampleDonorCsvRepository(records_path=RECORDS_DIR_PATH,
                                         separator=CSV_SEPARATOR,
-                                        donor_parsing_map=PARSING_MAP['donor_map'])
+                                        donor_parsing_map=PARSING_MAP['donor_map'],
+                                        miabis_on_fhir_model=miabis_on_fhir_model)
+
+    def create_biobank_repository(self) -> BiobankRepository:
+        return BiobankJSONRepository(biobank_json_file_path=BIOBANK_PATH)
--- persistence/factories/repository_factory.py
+++ persistence/factories/repository_factory.py
@@ -1,5 +1,6 @@
 import abc
 
+from persistence.biobank_repository import BiobankRepository
 from persistence.condition_repository import ConditionRepository
 from persistence.sample_collection_repository import SampleCollectionRepository
 from persistence.sample_donor_repository import SampleDonorRepository
@@ -13,13 +14,17 @@ class RepositoryFactory(abc.ABC):
         pass
 
     @abc.abstractmethod
-    def create_sample_collection_repository(self) -> SampleCollectionRepository:
+    def create_sample_collection_repository(self, miabis_on_fhir_model: bool = False) -> SampleCollectionRepository:
         pass
 
     @abc.abstractmethod
-    def create_sample_repository(self) -> SampleRepository:
+    def create_sample_repository(self, miabis_on_fhir_model: bool = False) -> SampleRepository:
         pass
 
     @abc.abstractmethod
-    def create_sample_donor_repository(self) -> SampleDonorRepository:
+    def create_sample_donor_repository(self, miabis_on_fhir_model: bool = False) -> SampleDonorRepository:
+        pass
+
+    @abc.abstractmethod
+    def create_biobank_repository(self) -> BiobankRepository:
         pass
