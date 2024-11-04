7fcb5be78a1990d37c144c62ef8c8958e974a75b
SimonKonar <simon.konar@gmail.com>
2024-11-04 14:27:12 +0100

    feat: ability to  also parse miabis_on_fhir model

--- persistence/sample_collection_repository.py
+++ persistence/sample_collection_repository.py
@@ -1,10 +1,11 @@
 import abc
 from typing import Generator
 
+from model.interface.collection_interface import CollectionInterface
 from model.sample_collection import SampleCollection
 
 
 class SampleCollectionRepository:
     @abc.abstractmethod
-    def get_all(self) -> Generator[SampleCollection, None, None]:
+    def get_all(self) -> Generator[CollectionInterface, None, None]:
         """Get all conditions."""
