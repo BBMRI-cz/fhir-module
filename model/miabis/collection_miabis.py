fe7a9320e9d8827c875759b76ea9a58be79699b3
SimonKonar <simon.konar@gmail.com>
2024-11-22 12:13:52 +0100

    feat: add miabis models implementation

--- model/miabis/collection_miabis.py
+++ model/miabis/collection_miabis.py
@@ -1,9 +1,9 @@
-from miabis_model import Collection, CollectionOrganization, Gender
+from miabis_model import Collection, Gender
 
 from model.interface.collection_interface import CollectionInterface
 
 
-class CollectionMiabis(CollectionOrganization, CollectionInterface):
+class CollectionMiabis(Collection, CollectionInterface):
     def __init__(self, identifier: str, name: str, managing_biobank_id: str,
                  contact_name: str, contact_surname: str,
                  contact_email: str,
@@ -19,13 +19,10 @@ class CollectionMiabis(CollectionOrganization, CollectionInterface):
                  material_types: list[str] = None):
         super().__init__(identifier=identifier, name=name, managing_biobank_id=managing_biobank_id,
                          contact_name=contact_name, contact_surname=contact_surname, contact_email=contact_email,
-                         country=country, alias=alias, url=url, description=description, dataset_type=dataset_type,
+                         country=country, genders=[Gender.UNKNOWN], material_types=material_types, number_of_subjects=0,
+                         alias=alias, url=url, description=description, dataset_type=dataset_type,
                          sample_source=sample_source, sample_collection_setting=sample_collection_setting,
                          collection_design=collection_design, use_and_access_conditions=use_and_access_conditions,
                          publications=publications)
-        self._collection = Collection(identifier=identifier, name=name, managing_collection_org_id=identifier,
-                                      genders=[Gender.UNKNOWN], material_types=material_types)
-
-    @property
-    def collection(self):
-        return self._collection
+        # self._collection = Collection(identifier=identifier, name=name, managing_collection_org_id=identifier,
+        #                               genders=[Gender.UNKNOWN], material_types=material_types)
