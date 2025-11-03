c165d8cb51639d33852a706d9a30f3812ff54ff5
Jonáš Jadrníček <514017@mail.muni.cz>
2025-11-03 17:15:19 +0100

    feat: mandatory password change after seed on UI
    Signed-off-by: Jonáš Jadrníček <514017@mail.muni.cz>

---
+++ ui/fhir-place/drizzle/0001_add_user_must_reset_password.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `users` ADD `must_reset_password` integer DEFAULT 0;
--- ui/fhir-place/src/actions/settings/changePassword.ts
+++ ui/fhir-place/src/actions/settings/changePassword.ts
@@ -3,10 +3,10 @@
 import { z } from "zod";
 import bcrypt from "bcryptjs";
 import { ChangePasswordSchema } from "@/app/(authorized)/settings/schema/ChangePasswordSchema";
-import { UserDetails } from "@/lib/auth-utils";
-import { users } from "@/lib/schema";
+import { UserDetails } from "@/lib/auth/auth-utils";
+import { users } from "@/lib/db/schema";
 import { eq } from "drizzle-orm";
-import { db } from "@/lib/db";
+import { db } from "@/lib/db/db";
 
 export async function changePassword(
   formData: z.infer<typeof ChangePasswordSchema>,
