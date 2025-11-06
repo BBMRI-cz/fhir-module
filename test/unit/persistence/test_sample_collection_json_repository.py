f75d023c34fab9ce58489946317bc08c9100255a
Jonáš Jadrníček <514017@mail.muni.cz>
2025-11-06 17:23:13 +0100

    feat: test fixes and config backward compatibility

--- test/unit/persistence/test_sample_collection_json_repository.py
+++ test/unit/persistence/test_sample_collection_json_repository.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 import pytest
@@ -25,6 +26,11 @@ class TestSampleCollectionRepository(unittest.TestCase):
 
     @patchfs
     def test_get_all_ok(self, fake_fs):
+        util_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))), 'util')
+        fake_fs.create_file(
+            os.path.join(util_dir, 'shared_config.json'),
+            contents='{"SAMPLE_COLLECTIONS_PATH": "default_sample_collection.json"}'
+        )
         fake_fs.create_file(get_sample_collections_path(), contents=self.collections_json)
         self.assertEqual(1, sum(1 for _ in self.sample_collection_repo.get_all()))
         self.assertEqual("test:collection:1", next(self.sample_collection_repo.get_all()).identifier)
